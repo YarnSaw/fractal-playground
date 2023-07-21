d9f876bfb26c0d1cc2827d39f19d6bae803c8738
Ryan Saweczko <yarn.sawe@gmail.com>
2023-07-20 20:12:54 -0400

    add module declaration to dcp package

--- dcpPackage.sh
+++ dcpPackage.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 
 # make sure most updated worker.js code is in package directory
+echo "module.declare([], function(require, exports, modules) {" >> package/fractals.js
 tail -n +2 worker.js | head -n -12 >> package/fractals.js
+echo -e "exports.mandelbrot=mandelbrot;\nexports.buddhabrot=buddhabrot;\nexports.newton=newton;\n})" >> package/fractals.js
 
 # update version number of package
 cd package
